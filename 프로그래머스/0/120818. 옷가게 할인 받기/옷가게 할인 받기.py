0125f25e791685bbbf8d4b7578f639eb3e4e2d9f
chewing0711 <168335110+chewing0711@users.noreply.github.com>
2024-05-01 16:02:03 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -0,0 +1,11 @@
+def solution(price):
+    if price >= 100000 and price < 300000 :
+        return int(price - (price * 0.05))
+    
+    elif price >= 300000 and price < 500000:
+        return int(price - price * 0.1)
+    
+    elif price >= 500000 :
+        return int(price - price * 0.2)
+    
+    return price
