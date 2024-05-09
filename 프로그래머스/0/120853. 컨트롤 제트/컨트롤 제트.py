29e4790c635a82b50c0ab93c8212b2acd6961702
chewing0711 <168335110+chewing0711@users.noreply.github.com>
2024-05-09 09:22:35 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.py
@@ -0,0 +1,11 @@
+def solution(s):
+    answer = []
+    
+    for i in s.split() :
+        
+        if i == "Z" :
+            answer.pop()
+        else :
+            answer.append(int(i))
+    
+    return sum(answer)
