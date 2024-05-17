2c843776d2ed0ea69b57aa312acde59b1e10fdae
chewing0711 <168335110+chewing0711@users.noreply.github.com>
2024-05-17 14:19:50 +0900

    [Bronze V] Title: 시험 성적, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,11 @@
+num = int(input())
+if (num>=90) :
+    print("A")
+elif (num>=80 and num<90) :
+    print("B")
+elif (num>=70 and num<80) :
+    print("C")
+elif (num>=60 and num<70) :
+    print("D")
+elif (num>=0 and num<60) :
+    print("F")
